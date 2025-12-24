7f8a69eaa9b49717d0848895162317a6348dcac6
6309-lcy <lcy20050603@gmail.com>
2025-12-24 22:09:47 +0800

    first-draft

---
+++ script.js
@@ -0,0 +1,14 @@
+var typed2 = new Typed('.text', {
+    strings:['Student --','Developer --','Designer --','Freelancer --'],
+    typeSpeed:100,
+    backSpeed:100,
+    cursorchar:'',
+    loop:true
+});
+var typed2 = new Typed('.self_text', {
+    strings:['Back-End Web Developer --','Designer --','Freelancer --'],
+    typeSpeed:100,
+    backSpeed:100,
+    cursorchar:'',
+    loop:true
+});
